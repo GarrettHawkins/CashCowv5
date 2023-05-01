9da9a647f76ffb5089af711a5a9859099593d8ac
Garrett Hawkins <hawkinsgarrett819@gmail.com>
2023-05-01 10:41:42 -0600

    Started Table View

--- CashCowv5/ViewController.swift
+++ CashCowv5/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    //Will need to pull from API
+    var stocks = ["S&P 500", "NASDAQ", "Dow Jones"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stocks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = stocks[indexPath.row]
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableViewOutlet.dataSource = self
     }
 
 
